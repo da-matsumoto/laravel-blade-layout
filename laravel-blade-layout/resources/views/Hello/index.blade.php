e0ccd040bdae02a07fa0349b46700595fda42d3e
da-matsumoto <da.mats1990@gmail.com>
2018-05-22 12:44:50 +0900

    @each

--- laravel-blade-layout/resources/views/Hello/index.blade.php
+++ laravel-blade-layout/resources/views/Hello/index.blade.php
@@ -9,17 +9,9 @@
 
 @section('content')
   <p>ここが本文コンテンツです。</p>
-  <p>必要なだけ記述できます。</p>
+  
+  @each('components.item', $data, 'item')
 
-  @component('components.message')
-    @slot('msg_title')
-    CAUTION!
-    @endslot
-
-    @slot('msg_content')
-    これはメッセージの表示です。
-    @endslot
-  @endcomponent
 @endsection
 
 @section('footer')
